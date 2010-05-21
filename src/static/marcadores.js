c473f75bb98915a3a689b53ab3f4184a33582d5b
Chester <cd@pobox.com>
2010-05-20 22:51:20 -0300

    marcadores agora sao coloridos e preservam a ordem objeto anonimo agora feito direito (chester burro: string != nome de propriedade)

--- src/static/marcadores.js
+++ src/static/marcadores.js
@@ -1,4 +1,6 @@
 var map;
+var ICON_URLS = ["", "http://www.google.com/mapfiles/marker_black.png", "http://www.google.com/mapfiles/marker_brown.png", "http://www.google.com/mapfiles/marker_green.png", "http://www.google.com/mapfiles/marker_purple.png", "http://www.google.com/mapfiles/marker_yellow.png", "http://www.google.com/mapfiles/marker_grey.png", "http://www.google.com/mapfiles/marker_orange.png", "http://www.google.com/mapfiles/marker_white.png"];
+
 var marcadores = {
 
     _marcadores: [],
@@ -7,26 +9,31 @@ var marcadores = {
         return _marcadores[id];
     },
     
-    add: function(latlng){
+    add: function(latlng, ordem){
         // Cria um novo marcador
+        if (!ordem) {
+            ordem = this.count() + 1;
+        }
         var id = new Date().getTime();
         marker = new google.maps.Marker({
             position: latlng,
             map: map,
-            draggable: true
+            draggable: true,
+            icon: ICON_URLS[ordem]
         });
+        m = this;
         google.maps.event.addListener(marker, "dragend", function(event){
             m.remove(id);
-            m.add(event.latLng);
+            m.add(event.latLng, ordem);
         });
         // Guarda ele na lista		
         this._marcadores[id] = {
-            "marker": marker,
-            "linhas": null,
-            "polylines": []
+            marker: marker,
+            linhas: null,
+            ordem: ordem,
+            polylines: [],
         }
         // Recupera as linhas que passam por ele (em background)
-        m = this;
         $.getJSON("/linhasquepassam.json", {
             lat: latlng.lat(),
             lng: latlng.lng()
@@ -60,19 +67,44 @@ var marcadores = {
     
     atualiza: function(){
         html = "";
-        for (i in this._marcadores) {
-            marcador = this._marcadores[i];
+        this.itera(function(marcador){
+            html += '<div style="clear:both;margin:4px;"><img src="' + ICON_URLS[marcador.ordem] + '" style="float:left;margin:2px;"/>';
             if (marcador.linhas) {
-                html += "Linhas:<br/>";
+                var vazio = true;
                 for (j in marcador.linhas) {
                     html += "- " + marcador.linhas[j].nome + "<br/>";
+                    vazio = false;
+                }
+                if (vazio) {
+                    html += "<br/>Nenhuma linha aqui";
                 }
             }
             else {
-                html += "Carregando..." + i + "<br/>";
+                html += "<br/>Carregando...";
+            }
+            html += "</div>";
+        })
+        $("#div_lista").html(html);
+    },
+    
+    itera: function(f){
+        var ms = this._marcadores;
+        var c = this.count();
+        for (i = 1; i <= c; i++) {
+            for (j in ms) {
+                if (ms[j].ordem == i) {
+                    f(ms[j]);
+                }
             }
         }
-        $("#div_lista").html(html);
+    },
+    
+    count: function(){
+        var j = 0;
+        for (marcador in this._marcadores) {
+            j++;
+        }
+        return j;
     },
     
     remove: function(id){
@@ -82,7 +114,7 @@ var marcadores = {
                 marcador.polylines[j].setMap(null);
             }
         }
-        marcador["marker"].setMap(null);
+        marcador.marker.setMap(null);
         delete this._marcadores[id];
         this.atualiza();
     }
