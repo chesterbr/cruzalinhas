62c6d32d91def1da2ba131458d78c62b40f373f0
Chester <cd@pobox.com>
2010-05-29 02:21:53 -0300

    Mudança dos scripts para carga assíncrona e script unificado onde possível Criado script python e builder do Eclipse/Aptana para gerar o script unificado Resolvido problema de tabs no marcadores.js

--- src/static/marcadores.js
+++ src/static/marcadores.js
@@ -1,11 +1,44 @@
+/*
+ * marcadores.js
+ * 
+ * Contém o código que gerencia os marcadores, carregando as linhas que passam por
+ * cada um deles e os dados de cada linha, além de lightboxes e outros detalhes de UI.
+ * 
+ * IMPORTANTE: Se você MODIFICAR esse arquivo, RODE o build_all_scripts.py, pois
+ * o html carrega o all_scripts.js e não esse.
+ * 
+ * (o Eclipse/Aptana está configurado com um builder apropriado, se tudo correr bem
+ * ele deve rodar automaticamente - chque se o .py acima está com permissão de
+ * execução).
+ * 
+ * Copyright (c) 2010 Carlos Duarte do Nascimento (Chester)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+ * software and associated documentation files (the "Software"), to deal in the Software 
+ * without restriction, including without limitation the rights to use, copy, modify, merge, 
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *     
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 // Evitando que alguém entre pelo html antigo
-if (location.href.indexOf("/static/")!=-1) {
-	location.replace("/");
+if (location.href.indexOf("/static/") != -1) {
+    location.replace("/");
 }
 
+// Gerenciamento dos marcadores
 var map;
-var ICON_URLS = ["", "http://www.google.com/mapfiles/marker_black.png", "http://www.google.com/mapfiles/marker_brown.png", "http://www.google.com/mapfiles/marker_green.png", "http://www.google.com/mapfiles/marker_purple.png", "http://www.google.com/mapfiles/marker_yellow.png", "http://www.google.com/mapfiles/marker_grey.png", "http://www.google.com/mapfiles/marker_orange.png", "http://www.google.com/mapfiles/marker_white.png"];
-var IMG_LOADER= '<img src="/static/ajax-loader.gif"/>';
+var ICON_URLS = ["", "http://www.google.com/mapfiles/marker_yellow.png", "http://www.google.com/mapfiles/marker_brown.png", "http://www.google.com/mapfiles/marker_green.png", "http://www.google.com/mapfiles/marker_purple.png", "http://www.google.com/mapfiles/marker_grey.png", "http://www.google.com/mapfiles/marker_orange.png", "http://www.google.com/mapfiles/marker_white.png"];
+var IMG_LOADER = '<img src="/static/ajax-loader.gif"/>';
 var LINHAS_INICIAL = 8;
 var LINHAS_INCREMENTO = 8;
 
@@ -35,12 +68,12 @@ var marcadores = {
             m.soft_remove(id);
             m.add(event.latLng, ordem);
         });
-        // Guarda ele na lista		
+        // Guarda ele na lista	
         this._marcadores[id] = {
-			id: id,
+            id: id,
             marker: marker,
             linhas: null,
-			max_linhas: LINHAS_INICIAL,
+            max_linhas: LINHAS_INICIAL,
             ordem: ordem,
             polylines: [],
         }
@@ -72,35 +105,36 @@ var marcadores = {
         this.atualiza();
         return id;
     },
-	    
+    
     atualiza: function(){
         var html = "";
         var count = this.count();
         for (key in this._cache_linhas) {
             this.apagaLinha(key);
         }
-		if (count==0) {
-			$("#div_lista").animate({width:'-=50'},0);
-			$("#div_lista").html($("#div_instrucoes").clone());
-			$("#div_lista").animate({width:'+=50'},1000);
-			return;			
-		}
+        if (count == 0) {
+            $("#div_lista").animate({
+                width: '-=50'
+            }, 0);
+            $("#div_lista").html($("#div_instrucoes").clone());
+            $("#div_lista").animate({
+                width: '+=50'
+            }, 1000);
+            return;
+        }
         for (ordem = 1; ordem <= count; ordem++) {
             var marcador = this.getMarcadorPelaOrdem(ordem);
-			if (!marcador) {
-				// estamos no meio de uma remoção, relaxa
-				return;
-			}
-            html += '<div style="margin-bottom:4px;clear:both;"><p style="margin:0px; text-align:center;"><img style="max-height:17px" src="' + ICON_URLS[marcador.ordem] + '"/><br/><span class="link_remover"><a class="link_remover" href="javascript:void(0)" onClick="javascript:marcadores.remove('+marcador.id+'); return false">REMOVER DO MAPA</a></span></p>';
+            if (!marcador) {
+                // estamos no meio de uma remoção, relaxa
+                return;
+            }
+            html += '<div style="margin-bottom:4px;clear:both;"><p style="margin:0px; text-align:center;"><img style="max-height:17px" src="' + ICON_URLS[marcador.ordem] + '"/><br/><span class="link_remover"><a class="link_remover" href="javascript:void(0)" onClick="javascript:marcadores.remove(' + marcador.id + '); return false">REMOVER DO MAPA</a></span></p>';
             if (marcador.linhas) {
                 var vazio = true;
-				var linhas_no_marcador = 0;
+                var linhas_no_marcador = 0;
                 for (j in marcador.linhas) {
                     var mostra = false;
                     var linha = marcador.linhas[j];
-//                    if (!this.passaMesmo(marcador, linha)) {
-//                        continue;
-//                    }
                     if (count == 1) {
                         // Só tem um marcador, mostra todas as linhas
                         mostra = true;
@@ -125,44 +159,50 @@ var marcadores = {
                         }
                     }
                     if (mostra) {
-						linhas_no_marcador++;
-						if (linhas_no_marcador > marcador.max_linhas) {
-							html+='<div style="clear:both"><p style="text-align:center;"><a href="javascript:void(0)" onClick="marcadores.mais('+marcador.id+');return false;">mais...</a></p></div>';
-							break;
-						}
-						var gif_loading = (this.desenhaLinha(linha) ? "&nbsp;" : IMG_LOADER);
-                        html +=  '<div class="legenda_linha" style="background-color:' +
-								 this.corDaLinha(linha) +
-								 '">' + gif_loading + '</div>' +
-								 '<p class="p_linha p_linha_' + 
-								 linha.key +
-								 '" style="float:left;margin:1px"><a href="' + 
-								 linha.url + 
-								 '" target="_blank">' + 
-								 marcadores.ajustaNome(linha.nome) + '</a>' + 
-								 '</p>';
+                        linhas_no_marcador++;
+                        if (linhas_no_marcador > marcador.max_linhas) {
+                            html += '<div style="clear:both"><p style="text-align:center;"><a href="javascript:void(0)" onClick="marcadores.mais(' + marcador.id + ');return false;">mais...</a></p></div>';
+                            break;
+                        }
+                        var gif_loading = (this.desenhaLinha(linha) ? "&nbsp;" : IMG_LOADER);
+                        html += '<div class="legenda_linha" style="background-color:' +
+                        this.corDaLinha(linha) +
+                        '">' +
+                        gif_loading +
+                        '</div>' +
+                        '<p class="p_linha p_linha_' +
+                        linha.key +
+                        '" style="float:left;margin:1px"><a href="' +
+                        linha.url +
+                        '" target="_blank">' +
+                        marcadores.ajustaNome(linha.nome) +
+                        '</a>' +
+                        '</p>';
                         vazio = false;
                     }
                 }
                 if (vazio) {
-					if (count == 1) {
+                    if (count == 1) {
                         html += this.pmsg("nenhuma linha passa aqui");
-                    } else if (ordem < count) {
-                        html += this.pmsg("nenhuma linha liga com o ponto abaixo");						
-					}
+                    }
+                    else 
+                        if (ordem < count) {
+                            html += this.pmsg("nenhuma linha liga com o ponto abaixo");
+                        }
                 }
             }
             else {
                 html += IMG_LOADER;
             }
-            html += "</div>";			
+            html += "</div>";
         }
-		if (count == 1) {
-			html += '<p class="p_dicas">Você pode arrastar o pino no mapa para ajustar o local.<br/><br/>Acrescente outros pinos para ver somente as linhas entre eles.<br/><br/>A busca não leva em conta os pontos de ônibus, apenas a proximidade do trajeto.</p>';
-		} else {
-			html += '<p class="p_dicas">Com dois ou mais pinos, só aparecem as linhas que passam entre eles.<br/><br/>Para apagar um pino, clique em REMOVER DO MAPA.<br/><br/>A busca não leva em conta os pontos de ônibus, apenas a proximidade do trajeto.</p>';				
-		}
-
+        if (count == 1) {
+            html += '<p class="p_dicas">Você pode arrastar o pino no mapa para ajustar o local.<br/><br/>Acrescente outros pinos para ver somente as linhas entre eles.<br/><br/>A busca não leva em conta os pontos de ônibus, apenas a proximidade do trajeto.</p>';
+        }
+        else {
+            html += '<p class="p_dicas">Com dois ou mais pinos, só aparecem as linhas que passam entre eles.<br/><br/>Para apagar um pino, clique em REMOVER DO MAPA.<br/><br/>A busca não leva em conta os pontos de ônibus, apenas a proximidade do trajeto.</p>';
+        }
+        
         $("#div_lista").html(html);
     },
     
@@ -183,17 +223,17 @@ var marcadores = {
         }
         return j;
     },
-
-	// Aumenta a quantidade de linhas mostrada num marcador e atualiza a listagem
-	mais: function(id) {
+    
+    // Aumenta a quantidade de linhas mostrada num marcador e atualiza a listagem
+    mais: function(id){
         var marcador = this._marcadores[id];
         if (marcador) {
-			marcador.max_linhas += LINHAS_INCREMENTO;
-		}
-		this.atualiza();
-	},
+            marcador.max_linhas += LINHAS_INCREMENTO;
+        }
+        this.atualiza();
+    },
     
-	// Tira um marcador da lista (mas não preenche o buraco dele)
+    // Tira um marcador da lista (mas não preenche o buraco dele)
     soft_remove: function(id){
         var marcador = this._marcadores[id];
         if (marcador.linhas) {
@@ -205,18 +245,18 @@ var marcadores = {
         delete this._marcadores[id];
         //this.atualiza();
     },
-	
-    remove: function(id) {
-		var ordem = this._marcadores[id].ordem;
-		for(i=this.count(); i>ordem;i--) {
-			var m = this.getMarcadorPelaOrdem(i);
-			this.soft_remove(m.id);
-			this.add(m.marker.position, i-1);
-		}
-		this.soft_remove(id);
-		this.atualiza();
-	},
-
+    
+    remove: function(id){
+        var ordem = this._marcadores[id].ordem;
+        for (i = this.count(); i > ordem; i--) {
+            var m = this.getMarcadorPelaOrdem(i);
+            this.soft_remove(m.id);
+            this.add(m.marker.position, i - 1);
+        }
+        this.soft_remove(id);
+        this.atualiza();
+    },
+    
     // Se a linha já está no cache desenha e retorna true
     // Caso não esteja (ou esteja mas ainda não tenha carregado), retorna false
     //   (e pede pra carregar se ainda não o fez)
@@ -233,7 +273,7 @@ var marcadores = {
                     gpontos.push(new google.maps.LatLng(pontos[j][0], pontos[j][1]));
                 }
                 cache[linha.key].pontos = gpontos;
-				cache[linha.key].cor = marcadores.proximacor();
+                cache[linha.key].cor = marcadores.proximacor();
                 marcadores.atualiza();
             });
         }
@@ -246,12 +286,12 @@ var marcadores = {
                     strokeWeight: 5,
                     strokeOpacity: 0.5
                 });
-				google.maps.event.addListener(c.polyline, "mouseover", function(latlng) {
-					$(".p_linha_"+linha.key).addClass("destaca_linha");
-				});
-				google.maps.event.addListener(c.polyline, "mouseout", function(latlng) {
-					$(".p_linha_"+linha.key).removeClass("destaca_linha");
-				});
+                google.maps.event.addListener(c.polyline, "mouseover", function(latlng){
+                    $(".p_linha_" + linha.key).addClass("destaca_linha");
+                });
+                google.maps.event.addListener(c.polyline, "mouseout", function(latlng){
+                    $(".p_linha_" + linha.key).removeClass("destaca_linha");
+                });
             }
             return true;
         }
@@ -266,56 +306,60 @@ var marcadores = {
             delete c.polyline;
         }
     },
-	
-	pmsg: function(texto) {
-		return '<p style="color:red;margin:0px;font-style:italic;text-align:center;">'+texto+"</p>";
-	},
-	
-	_semente_cor: 1,
-	proximacor: function() {
-		this._semente_cor+=3;
-		if (this._semente_cor > 62) {
-			this._semente_cor -= 62;
-		}
-		var h = this._semente_cor.toString(4);
-   		while (h.length<3) h="0"+h;
-   		var c = h[0]*256*4+h[1]*16*4+h[2]*4;
-   		cor = c.toString(16)
-   		while (cor.length<3) cor="0"+cor;
-		return "#"+cor;
-	},
-	
-	corDaLinha: function(linha) {
+    
+    pmsg: function(texto){
+        return '<p style="color:red;margin:0px;font-style:italic;text-align:center;">' + texto + "</p>";
+    },
+    
+    _semente_cor: 1,
+    proximacor: function(){
+        this._semente_cor += 3;
+        if (this._semente_cor > 62) {
+            this._semente_cor -= 62;
+        }
+        var h = this._semente_cor.toString(4);
+        while (h.length < 3) 
+            h = "0" + h;
+        var c = h[0] * 256 * 4 + h[1] * 16 * 4 + h[2] * 4;
+        cor = c.toString(16)
+        while (cor.length < 3) 
+            cor = "0" + cor;
+        return "#" + cor;
+    },
+    
+    corDaLinha: function(linha){
         var c = this._cache_linhas[linha.key];
-		if (c && c.cor) {
-			return c.cor;
-		} else {
-			return "#FFF";
-		}
-   		
-	},
-	
-	// Deixa o nome da linha menos feio
-	ajustaNome: function(nome) {
-		// Converte 1a. letra pra upper, outras pra lower
-		var baixar = false;
-		var result = "";
-		for (i = 0; i < nome.length; i++) {
-			var c = nome[i];
-			if (baixar) {
-				result += c.toLowerCase();
-			} else {
-				result += c;
-			}
-			baixar = (c.toLowerCase()!=c);		
-		}
-		// Separa ida e volta
-		return result.replace("/"," / ");
-	}
-
-
-
-
+        if (c && c.cor) {
+            return c.cor;
+        }
+        else {
+            return "#FFF";
+        }
+        
+    },
+    
+    // Deixa o nome da linha menos feio
+    ajustaNome: function(nome){
+        // Converte 1a. letra pra upper, outras pra lower
+        var baixar = false;
+        var result = "";
+        for (i = 0; i < nome.length; i++) {
+            var c = nome[i];
+            if (baixar) {
+                result += c.toLowerCase();
+            }
+            else {
+                result += c;
+            }
+            baixar = (c.toLowerCase() != c);
+        }
+        // Separa ida e volta
+        return result.replace("/", " / ");
+    }
+    
+    
+    
+    
     
 }
 
@@ -324,59 +368,69 @@ function inicializa(){
     centro_sp = new google.maps.LatLng(-23.548153, -46.633101);
     map = new google.maps.Map(document.getElementById("map_canvas"), {
         zoom: 13,
-		scaleControl: true,
+        scaleControl: true,
         center: centro_sp,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     google.maps.event.addListener(map, "click", function(event){
-		if (marcadores.count()<=5) {
-        	marcadores.add(event.latLng);
-		} else {
-			alert("Desculpe, não consigo colocar mais marcadores");
-		}
+        if (marcadores.count() <= 5) {
+            marcadores.add(event.latLng);
+        }
+        else {
+            alert("Desculpe, não consigo colocar mais marcadores");
+        }
     });
-	$("#text_busca").focus(function () {
-		if ($(this).hasClass("input_instrucoes")) {
-			$(this).removeClass("input_instrucoes");
-			$(this).attr("value", "");
-		}
-	});
-
-	marcadores.atualiza();
+    $("#text_busca").focus(function(){
+        if ($(this).hasClass("input_instrucoes")) {
+            $(this).removeClass("input_instrucoes");
+            $(this).attr("value", "");
+        }
+    });
+    
+    marcadores.atualiza();
 }
 
 
-$(document).ready(function(){
-	$("#link_oque").fancybox({
-		'width'				: 630,
-		'height'			: 340,
-		'autoDimensions'	: false,
-		'autoScale'			: false
-	});
-	$("#link_porque").fancybox({
-		'width'				: 610,
-		'height'			: 340,
-		'autoDimensions'	: false,
-		'autoScale'			: false
-	});
-	$("#link_como").fancybox({
-		'width'				: 630,
-		'height'			: 390,
-		'autoDimensions'	: false,
-		'autoScale'			: false
-	});
-	$("#link_sobre").fancybox({
-		'width'				: 500,
-		'height'			: 340,
-		'autoDimensions'	: false,
-		'autoScale'			: false
-	});
 
+
+$(document).ready(function(){
+    $("#link_oque").fancybox({
+        'width': 630,
+        'height': 340,
+        'autoDimensions': false,
+        'autoScale': false
+    });
+    $("#link_porque").fancybox({
+        'width': 610,
+        'height': 340,
+        'autoDimensions': false,
+        'autoScale': false
+    });
+    $("#link_como").fancybox({
+        'width': 630,
+        'height': 390,
+        'autoDimensions': false,
+        'autoScale': false
+    });
+    $("#link_sobre").fancybox({
+        'width': 500,
+        'height': 340,
+        'autoDimensions': false,
+        'autoScale': false,
+        'onComplete': function(){
+            // Delay nesse botão que demora a vida pra carregar
+            // (e não me pergunte por que ele só carrega direito na segunda vez)
+            var botao = '<iframe width="50" scrolling="no" height="60" frameborder="0" src="http://api.flattr.com/button/view/7569643d302675726c3d68747470253341253246253246636865737465722e626c6f672e6272266c6e673d26686964653d30267469746c653d266361743d267461673d26646573633d" border="0" marginheight="0" marginwidth="0" allowtransparency="true"></iframe>';
+            $('#div_botao_flattr').html(botao);
+            $('#div_botao_flattr').html(botao);
+        }
+    });
+    
     inicializa();
     $('#form_busca').submit(function(){
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode({
-            'address': $('#text_busca').val()+", São Paulo, SP"
+            'address': $('#text_busca').val() + ", São Paulo, SP"
         }, function(results, status){
             if (status == google.maps.GeocoderStatus.OK) {
                 latlng = results[0].geometry.location;
