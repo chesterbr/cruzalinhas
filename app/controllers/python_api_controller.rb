fa84b23ed5af23a40db0d9479490cfbeaae1bdb0
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2014-10-03 23:47:28 -0400

    Mostly working API (missing URL only) \o/

--- app/controllers/python_api_controller.rb
+++ app/controllers/python_api_controller.rb
@@ -1,17 +1,26 @@
 class PythonApiController < ApplicationController
-
   def linhasquepassam
-    render json: [{"url"=>"http://200.99.150.170/PlanOperWeb/detalheLinha.asp?TpDiaID=0&CdPjOID=12967", "hashes"=>["6gyf5f", "6gyfn4", "6gyfn5", "6gyfn6", "6gyf49", "6gyf46", "6gyf47", "6gyfh7", "6gyfh4", "6gyf51", "6gyfju", "6gyf4j", "6gyf4k", "6gyfjs", "6gyf4d", "6gyf4b", "6gyfhg", "6gyfjg", "6gyfjh", "6gyf50", "6gyf53", "6gyfjk"], "key"=>1295, "nome"=>"METRÔ L3-0 CORINTHIANS - ITAQUERA/PALMEIRAS - BARRA FUNDA"}, {"url"=>"http://200.99.150.170/PlanOperWeb/detalheLinha.asp?TpDiaID=0&CdPjOID=59661", "hashes"=>["6gyf4f", "6gyf4g", "6gyf4d", "6gyccm", "6gycch", "6gycct", "6gyf49", "6gycfp", "6gyf42", "6gyf43", "6gyf40", "6gyccy"], "key"=>1296, "nome"=>"METRÔ L4-0 BUTANTA/LUZ"}]
+    geohash = GeoHash.encode params[:lat].to_f,
+                             params[:lng].to_f,
+                             Rails.configuration.geohash_precision
+    trips = TripGeohash.includes(:trip, :trip => :route).where(:geohash => geohash).map { |h|
+      {
+        url: "http://200.99.150.170/PlanOperWeb/detalheLinha.asp?TpDiaID=0&CdPjOID=12967",
+        hashes: TripGeohash.where(trip: h.trip).pluck(:geohash),
+        key: h.trip_id,
+        nome: "#{h.trip.route.route_short_name}: #{h.trip.route.route_long_name}"
+      }
+    }
+
+    render json: trips
   end
 
   def linha
-    route  = GtfsEngine::Route.find(params[:key])
-    trip   = route.trips.first
+    trip   = GtfsEngine::Trip.find(params[:key])
     shapes = trip.shapes.order(:shape_pt_sequence).
              select(:shape_pt_lat, :shape_pt_lon)
     points = shapes.map { |s| [s.shape_pt_lat, s.shape_pt_lon] }
 
     render json: points
   end
-
 end
