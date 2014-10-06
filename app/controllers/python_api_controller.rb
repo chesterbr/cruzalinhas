7d63fa49707da2465c1b30cdbac1bab7625cca43
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2014-10-05 23:51:35 -0400

    Use SPTrans url again (not direct, but works!)

--- app/controllers/python_api_controller.rb
+++ app/controllers/python_api_controller.rb
@@ -5,7 +5,7 @@ class PythonApiController < ApplicationController
                              Rails.configuration.geohash_precision
     trips = TripGeohash.includes(:trip, :trip => :route).where(:geohash => geohash).map { |h|
       {
-        url: "http://www.toape.com.br/#{h.trip.route.route_short_name}/",
+        url: h.sptrans_url,
         hashes: TripGeohash.where(trip: h.trip).pluck(:geohash),
         key: h.trip_id,
         nome: "#{h.trip.route.route_short_name}: #{h.trip.route.route_long_name}"
--- app/models/trip_geohash.rb
+++ app/models/trip_geohash.rb
@@ -1,3 +1,10 @@
 class TripGeohash < ActiveRecord::Base
   belongs_to :trip, class_name: "GtfsEngine::Trip"
+
+  def sptrans_url
+    text_id = trip.route.route_short_name
+    sanitized_id = CGI::escape(I18n.transliterate(text_id))
+
+    "http://200.99.150.170/PlanOperWeb/linhaselecionada.asp?Linha=#{sanitized_id}"
+  end
 end
