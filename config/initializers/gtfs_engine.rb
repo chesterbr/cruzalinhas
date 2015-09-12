5693f5c55e1ce7841400eba7896468cbd82315c5
Carlos Duarte do Nascimento (Chester) <cd@pobox.com>
2015-09-12 06:34:05 +0000

    use local sptrans gtfs file

--- config/initializers/gtfs_engine.rb
+++ config/initializers/gtfs_engine.rb
@@ -1,6 +1,6 @@
 GtfsEngine.sources do |source|
   source.sptrans do
     title 'SPTrans'
-    url 'http://cruzalinhas.com/gtfs-sptrans.zip'
+    url '/home/chester/cruzalinhas/source/public/gtfs-sptrans.zip'
   end
 end
