9e52d793900483deeb292842c989cdf4b77096ac
vinicius834 <vinicius834@gmail.com>
2015-12-30 23:56:38 -0200

    correção assets

--- app/controllers/python_api_controller.rb
+++ app/controllers/python_api_controller.rb
@@ -11,6 +11,7 @@ class PythonApiController < ApplicationController
         nome: "#{h.trip.route.route_short_name}: #{h.trip.route.route_long_name}"
       }
     }
+
     render json: trips
   end
 
